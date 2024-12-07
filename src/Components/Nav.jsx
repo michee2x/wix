0015d2f9c5ebe2ab97167f68b96a87e62f691fff
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-12-07 22:27:54 +0100

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,16 +5,16 @@ const Nav = () => {
     const [isOpen, setIsOpen] = useState(false)
     const {showSideBar, setShowSideBar} = ContextAPI()
   return (
-    <nav className="px-2 py-4 shadow">
+    <nav className="pl-3 py-4 shadow">
         <div className="lg:items-center lg:justify-between lg:flex">
             <div className="flex items-center justify-between">
                 <a href="/" className="">
-                    <code className='text-gray-800 flex gap-2 items-center text-xl font-extrabold translate-x-9'><img class="w-auto transform rotate-180 h-6" src="https://merakiui.com/images/logo.svg" alt="" /><span className="">i x y</span></code>
+                    <code className='text-gray-800 flex gap-2 items-center text-xl font-extrabold translate-x-9'><img class="w-auto transform rotate-180 h-6" src="https://merakiui.com/images/logo.svg" alt="" /><span className=""> i x y</span></code>
                 </a>
 
                 {/* <!-- Mobile menu button --> */}
                 <div className="lg:hidden">
-                    <button onClick={() => setShowSideBar(true)} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <button onClick={() => setShowSideBar(true)} type="button" className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
                         <svg xmlns="http://www.w3.org/2000/svg" className={`${!isOpen ? "block" : "hidden"} w-6 h-6`} fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
                         </svg>
