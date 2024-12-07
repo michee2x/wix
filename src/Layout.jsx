df03e5c1a25de859a69a8b15a24633e801b62b32
michael israel <michaelisraelmike@gmail.com>
2024-12-08 00:58:32 +0100

    added sidebar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Footer } from './Components/Footer'
 import Nav from './Components/Nav'
+import { SideBar } from './Components/SideBar'
 
 const Layout = () => {
   return (
     <div className='w-screen min-h-screen'>
     <Nav />
-    <Outlet />
-    <Footer />
+    <SideBar Outlet={<Outlet />} />
     </div>
   )
 }
