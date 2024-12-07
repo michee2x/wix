5c20a436af8d7a4e94599abfcc0e1af3d59eab9d
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-12-07 22:23:22 +0100

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
         <div className="lg:items-center lg:justify-between lg:flex">
             <div className="flex items-center justify-between">
                 <a href="/" className="">
-                    <code className='text-gray-500 flex gap-2 items-center text-xl font-extrabold translate-x-9'><img class="w-auto transform rotate-180 h-6" src="https://merakiui.com/images/logo.svg" alt="" /><span className="">i x y</span></code>
+                    <code className='text-gray-800 flex gap-2 items-center text-xl font-extrabold translate-x-9'><img class="w-auto transform rotate-180 h-6" src="https://merakiui.com/images/logo.svg" alt="" /><span className="">i x y</span></code>
                 </a>
 
                 {/* <!-- Mobile menu button --> */}
