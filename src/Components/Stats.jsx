ec32ef1c82dbc5f8f235ee397b9381fc0c0a75b9
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-12-09 11:34:50 +0100

    Update Stats.jsx

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -18,13 +18,13 @@ const Stats = () => {
 }, [])
   return (
     <>
-      <div class="bg-white py-6 sm:py-8 lg:py-12">
+      <div class="bg-white dark:bg-gray-900 py-6 sm:py-8 lg:py-12">
   <div class="mx-auto max-w-screen-xl px-4 md:px-8">
     {/* <!-- text - start --> */}
     <div class="mb-10 md:mb-16">
-      <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">Our Team by the numbers</h2>
+      <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl dark:text-gray-100">Our Team by the numbers</h2>
 
-      <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
+      <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg dark:text-gray-200">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
     </div>
     {/* <!-- text - end --> */}
 
@@ -32,27 +32,27 @@ const Stats = () => {
       {/* <!-- stat - start --> */}
       <div data-aos="fade-up" class="flex flex-col items-center md:p-4">
         <div class="text-xl font-bold text-indigo-500 sm:text-2xl md:text-3xl"><CountComponent num={200}/>+</div>
-        <div class="text-sm font-semibold sm:text-base">People</div>
+        <div class="text-sm font-semibold sm:text-base dark:text-gray-100">People</div>
       </div>
       {/* <!-- stat - end --> */}
 
       {/* <!-- stat - start --> */}
       <div data-aos="fade-up" class="flex flex-col items-center md:p-4">
         <div class="text-xl font-bold text-indigo-500 sm:text-2xl md:text-3xl" data-aos="fade-down"><CountComponent num={500}/>+</div>
-        <div class="text-sm font-semibold sm:text-base">People</div>
+        <div class="text-sm font-semibold sm:text-base dark:text-gray-100">People</div>
       </div>
 
       {/* <!-- stat - start --> */}
       <div data-aos="fade-up" class="flex flex-col items-center md:p-4">
         <div class="text-xl font-bold text-indigo-500 sm:text-2xl md:text-3xl"><CountComponent num={1000}/>+</div>
-        <div class="text-sm font-semibold sm:text-base">Customers</div>
+        <div class="text-sm font-semibold sm:text-base dark:text-gray-100">Customers</div>
       </div>
       {/* <!-- stat - end --> */}
 
       {/* <!-- stat - start --> */}
       <div data-aos="fade-up" class="flex flex-col items-center md:p-4">
         <div class="text-xl font-bold text-indigo-500 sm:text-2xl md:text-3xl">A couple</div>
-        <div class="text-sm font-semibold sm:text-base">Coffee breaks</div>
+        <div class="text-sm font-semibold sm:text-base dark:text-gray-100">Coffee breaks</div>
       </div>
       {/* <!-- stat - end --{'>'} */}
     </div>
