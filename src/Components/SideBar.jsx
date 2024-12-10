a787c37186c424c4745121c86708bf00570321ff
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-12-10 01:29:36 +0100

    Update SideBar.jsx

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -32,8 +32,8 @@ export const SideBar = ({children}) => {
 
     <div class="flex flex-col justify-between flex-1 mt-6">
         <nav class="flex-1 -mx-3 space-y-3 ">
-            <div class="relative mx-3">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+            <div class="relative hidden mx-3">
+                <span class="absolute hidden inset-y-0 left-0 flex items-center pl-3">
                     <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
                         <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                     </svg>
